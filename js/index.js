fd242b5106c3a79b18a237199883a112ac3e1d1e
Mitchell Budden <mitchbudden79@gmail.com>
2017-02-17 11:38:09 +1100

    2/17/17 update

--- js/index.js
+++ js/index.js
@@ -8,14 +8,4 @@ $(document).ready(function(){
     }
   });
   
-  //Contact variables
-  var name = $("#leavenamehere").val();
-  var email = $("#leaveemailhere").val();
-  var phone = $("#leavephonehere").val();
-  var message = $("#leavemessagehere").val();
-  
-  $("#send").on('click', function() {
-    console.log(name + email + phone + message);
-  })
-  
 })
