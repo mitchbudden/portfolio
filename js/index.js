d095b23653d5709635d7569c004305c4cad65f69
Mitchell Budden <mitchbudden79@gmail.com>
2018-02-01 20:04:42 -0500

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){ 
+  $(window).scroll(function () {  
+    if ($(window).scrollTop() > -1) {
+      $('.navbar').addClass('navbar-fixed');
+    }
+    if ($(window).scrollTop() < 0) {
+      $('.navbar').removeClass('navbar-fixed');
+    }
+  });
+  
+})
